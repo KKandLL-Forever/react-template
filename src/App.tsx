821e35c4a1728b71484b9438a65ff6ddac2e5ef7
KKandLL-Forever <clay1wang@gmail.com>
2022-07-18 17:12:48 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import '@/App.scss'
+
+function App () {
+  render () {
+    return (
+      <div className="App">
+        React app
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/store/root.Store.ts
@@ -0,0 +1,11 @@
+import { makeAutoObservable } from 'mobx'
+
+class RootStore {
+
+  constructor () {
+    makeAutoObservable(this)
+  }
+  
+}
+
+export const rootStore = new RootStore()
